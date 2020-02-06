f4f44d6b03c39215050259c78d0d03d837de59ee
eli <46949964+securityELI@users.noreply.github.com>
2020-02-06 12:51:43 -0600

    Create cveToSnort.py

---
+++ cveToSnort.py
@@ -0,0 +1,21 @@
+CVE_Collection = [line.rstrip('\n') for line in open('cve')]
+length = len(CVE_Collection)
+f = open('sidList.txt','w')
+f.close()
+
+for i in range (0, length):
+        cve = CVE_Collection[i]
+
+        url = "https://www.snort.org/rule_docs?utf8=%E2%9C%93&rules_query=" + cve + "&search_type=standard&submit_rule_search="
+        r = requests.get(url)
+
+        if 'Search returned' in r.text:
+                a = r.text
+                pre = a.rfind('<span class="categories">')
+                index2 = a.find('<',pre + len('<span class="categories">'))
+                SID = (a[pre + len('<span class="categories">'):index2])
+
+
+                print (cve + " - SID has been found. Storing: " + SID )
+                with open('sidList.txt', 'a') as afile:
+                        afile.write(SID+'\n')
