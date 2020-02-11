ba9cf6c48731c7fd3f3f2639feaf61c89112dafc
eli <46949964+securityELI@users.noreply.github.com>
2020-02-11 12:16:55 -0600

    Update c2snort.py

--- c2snort.py
+++ c2snort.py
@@ -1,22 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 
 #Remember to have a file named "cve" which stores all your CVEs, line by line.
 CVE_Collection = [line.rstrip('\n') for line in open('cve')]
 length = len(CVE_Collection)
 f = open('sidList.txt','w')
 f.close()
-
-# for statement that goes through all CVEs line by line
+sid = {}
 for i in range (0, length):
-    cve = CVE_Collection[i]
-    url = "https://www.snort.org/rule_docs?utf8=%E2%9C%93&rules_query=" + cve + "&search_type=standard&submit_rule_search="
-        r = requests.get(url)
-        
-       if 'Search returned' in r.text:
-            a = r.text
-                pre = a.rfind('<span class="categories">')
-            index2 = a.find('<',pre + len('<span class="categories">'))
-            SID = (a[pre + len('<span class="categories">'):index2])
-            print (cve + " - SID has been found. Storing: " + SID )
-            with open('sidList.txt', 'a') as afile:
-            afile.write(SID+'\n')
+	cve = CVE_Collection[i]
+	url = "https://www.snort.org/rule_docs?utf8=%E2%9C%93&rules_query=" + cve + "&search_type=standard&submit_rule_search="
+	x = requests.get(url)	
+    
+   	if 'Search returned' in x.text:
+		soup = BeautifulSoup(x.text,'lxml')
+		for tag in soup.find_all('span',{"class":"categories"}):
+			#sid = str(tag.text)
+			with open('sidList.txt','a') as afile:
+				afile.write(tag.text +'\n')
+				print ("SID: " + tag.text + " found.") 
